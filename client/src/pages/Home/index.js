147601a7a0de729378fb350b63c0759060cc28cd
James Geib <jpgeib2@gmail.com>
2025-02-08 10:25:21 -0500

    set up grid structure for Home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Calendar from "../../components/Calendar";
+import PatientList from "../../components/PatientList";
+import PatientInfo from "../../components/PatientInfo";
 
 import "./style.css";
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div id="home-container">
+            <div id="home">
+                <div className="home-col">
+                    <PatientInfo />
+                    <Calendar />
+                </div>
+                <div className="home-col">
+                    <PatientList />
+                </div>
+            </div>
         </div>
     );
 };
