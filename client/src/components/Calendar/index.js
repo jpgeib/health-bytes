63e947556076e2bcc86dba30aab34bf39577c448
James Geib <jpgeib2@gmail.com>
2025-02-08 10:36:17 -0500

    got started on Calendar

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Header } from "semantic-ui-react";
 
 import "./style.css";
 
 const Calendar = () => {
+
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const [date, setDate] = useState(new Date());
+
     return (
         <div>
-            Calendar
+            <Header as="h1">Calendar</Header>
+            <Header as="h2">{months[setDate(date.getMonth())]} {setDate(date.getFullYear())}</Header>
         </div>
     );
 };
